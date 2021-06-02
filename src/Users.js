522155f8b23c2f42efc70622c64e0f58f3ff927a
jangji96 <xxwlgns@naver.com>
2021-06-02 16:15:05 +0900

    react-async 로 요청 상태 관리하기

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import {useAsync} from 'react-async';
 import User from './User'
 
 // function reducer(state, action) {
@@ -43,7 +43,11 @@ function Users() {
     //     error: null
     // });
 
-    const [state, refetch] = useAsync(getUsers, [], true);
+    // const [state, refetch] = useAsync(getUsers, [], true);
+
+    const { data: users, error, isLoading, run } = useAsync({
+        deferFn: getUsers
+    })
 
     // const [users, setUsers] = useState(null);
     // const [loading, setLoading] = useState(null);
@@ -73,11 +77,11 @@ function Users() {
     //     fetchUsers();
     // },[]);
 
-    const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
+    // const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
 
-    if (loading) return <div>로딩중..</div>;
+    if (isLoading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={run}>불러오기</button>;
     return (
         <>
             <ul>
@@ -91,7 +95,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={run}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
